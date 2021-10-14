c4285d900cfafbdc5f877133c44924ae2a2a11ef
Peter Nijssen <peter@peternijssen.nl>
2021-10-14 14:45:20 +0200

    Update client version

--- spiderpy/spiderapi.py
+++ spiderpy/spiderapi.py
@@ -257,7 +257,7 @@ class SpiderApi(object):
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
             'X-Client-Platform': 'android-phone',
-            'X-Client-Version': '1.5.3 (3561)',
+            'X-Client-Version': '1.5.9 (3611)',
             'X-Client-Library': 'SpiderPy'
         }
 
@@ -292,7 +292,7 @@ class SpiderApi(object):
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
             'X-Client-Platform': 'android-phone',
-            'X-Client-Version': '1.5.3 (3561)',
+            'X-Client-Version': '1.5.9 (3611)',
             'X-Client-Library': 'SpiderPy'
         }
 
